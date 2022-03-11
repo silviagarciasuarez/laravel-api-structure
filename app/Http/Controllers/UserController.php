d505c7824b4147c865fc07055f7446fe00f2f6d1
silviagarciasuarez <silviagarciasuarez27@gmail.com>
2022-03-11 10:29:44 +0100

    nnn

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,12 +51,15 @@ class UserController extends Controller
         return response()->json([
             "status" => "OK",
             "nombre" => $request->nombre,
+            "apellidos" => $request->apellidos,
             "email" => $request->email
             //probar a devolver también el campo apellidos
         ]);
     }
     public function postOtherThing(Request $request)
     {
+
+        echo "otherthing";
         // Crear una función en la que si se pasa el parámetro "premium" a true en el $request, 
         // devuelva un código de estado "OK" y un mensaje "el usuario es premium". 
         // En caso de que se le pase premium false, devolver un código de estado "KO" y mensaje "error: usuario no premium"
